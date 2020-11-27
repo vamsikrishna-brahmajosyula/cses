a99052f2d31a47c7f8be6dd5f4ece18d062494df
Vamsi Krishna Brahmajosyula <vamsikrishna.brahmajosyula@gmail.com>
2020-11-27 19:41:07 +0530

    Chessboard and Queens

--- cpp/chessboard_and_queens.cc
+++ cpp/chessboard_and_queens.cc
@@ -3,56 +3,38 @@
 using namespace std;
 
 int count = 0;
-const int n = 4;
-char board[n][n];
-
-
-bool isValid(int row, int col) {
-
-  int i, j; 
-  
-  /* Check this row on left side */
-  for (i = 0; i < col; i++) 
-      if (board[row][i] == 'q') 
-          return false; 
-
-  /* Check upper diagonal on left side */
-  for (i=row, j=col; i>=0 && j>=0; i--, j--) 
-      if (board[i][j] == 'q') 
-          return false; 
-
-  /* Check lower diagonal on left side */
-  for (i=row, j=col; j>=0 && i< n; i++, j--) 
-      if (board[i][j] == 'q') 
-          return false; 
-
-  return true;
-}
+bool reserved[8][8];
+bool cols[16], diag1[16], diag2[16];
 
 void solve(int i) {
-  if (i == n) {
+  if (i == 8) {
     count++;
     return;
   }
 
-  for (int j = 0; j < n; j++) {
-    if (board[i][j] != '*' && isValid(i, j)) {
-      board[i][j] = 'q';
-	    solve(i+1);
-      board[i][j] = '.';
-    }
+  for (int j = 0; j < 8; j++) {
+    if (cols[j] || diag1[i+j] || diag2[i-j+7] || reserved[i][j])
+      continue;
+    cols[j] = diag1[i+j] = diag2[i-j+7] = true;
+    solve(i+1);
+    cols[j] = diag1[i+j] = diag2[i-j+7] = false;
   }
 
 }
 
 int main(int argc, char* argv[]) {
-    string line;
-    for (int i = 0; i < n; i++) {
-        getline(cin, line);
-        for (int j = 0; j < n; j++) {
-            board[i][j] = line[j];
+    char x;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            cin >> x;
+            if (x == '*') {
+              reserved[i][j] = true;
+            } else {
+              reserved[i][j] = false;
+            }
         }
     }
+    count = 0;
     solve(0);
     cout << count << endl;
     return 0;
