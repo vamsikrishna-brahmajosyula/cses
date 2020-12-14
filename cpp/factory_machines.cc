aeb82d45611c5b5162efc64e75e4189bed56156c
Vamsi Krishna Brahmajosyula <vamsikrishna.brahmajosyula@gmail.com>
2020-12-14 16:28:56 +0530

    Factory Machines

--- cpp/factory_machines.cc
+++ cpp/factory_machines.cc
@@ -1,25 +1,52 @@
 #include <iostream>
-
-using namespace std;
+#include <vector>
+#include <queue>
 
 typedef long long ll;
 
-int main(int argc, char* argv[]) {
+using namespace std;
+
+int main(int argc, char *argv[])
+{
 
   ll t;
-  int n;
-  
-  vector<ll> times;
+  long n;
 
   cin >> n >> t;
   ll time;
-  
-  for (int i = 0; i < n; i++) {
+
+  vector<ll> k(n);
+
+  for (long i = 0; i < n; i++)
+  {
     cin >> time;
-    times.push_back(time);    
+    k[i]= time;
   }
 
-  
-  return 0;
+  ll min_time = 0;
 
+  ll start = 0;
+  ll end = 1e18;
+
+  while (start <= end) {
+    ll mid = (start + end) / 2;
+
+    ll sum = 0;
+
+    for (ll i = 0; i < n; i++) {
+      sum += (mid / k[i]);
+      if (sum >= t) {
+        break;
+      }
+    }
+
+    if (sum >= t) {
+      min_time = mid;
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  cout << min_time << endl;
+  return 0;
 }
